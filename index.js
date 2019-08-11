f9843d166bb065a8deafc78278db084a58c4c7bb
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2019-08-10 20:58:22 -0700

    a simple speech to text app

---
+++ index.js
@@ -0,0 +1,32 @@
+        // window.SpeechRecognition =window.SpeechRecognition || window.webkitSpeechRecognition;
+
+
+        // const recognition =new SpeechRecognition;
+        // recognition.interimResults =true;
+        // var display = document.querySelector('.white-o');
+        
+        // let firstp=document.querySelector('p')
+        // let p = document.createElement('p');
+        // const words =document.querySelector('.words');
+        // //words.appendChild(p);
+
+        // recognition.addEventListener('result', e => {
+        //     console.log(e.results);
+        //     const transcript =Array.from(e.results).map(result => result.transcript).join('');           
+        //     firstp.textContent += " " + e.results[0][0].transcript;
+        //     //console.log(e.results[0][0].transcript);
+        //     console.log(transcript);
+
+        //     if(e.results[0][0].isFinal){            
+        //         p = document.createElement('p');
+        //         words.appendChild(p);
+        //     }
+        //     if(transcript.includes('unicorn')){
+        //         console.log('unicorn');
+        //     }
+           
+        //     console.log(transcript);
+        // });
+        // recognition.addEventListener('end', recognition.start);
+
+        // recognition.start();
